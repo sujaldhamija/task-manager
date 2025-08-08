03ba81733e5bb8a913f164edb4a777e3ed42250d
Sujal Dhamija <sujaldhamija@gmail.com>
2025-08-08 21:05:20 +0530

    DB credentials stored in env

--- app/mysql_con.py
+++ app/mysql_con.py
@@ -1,11 +1,15 @@
-import mysql.connector
+import mysql.connector, os
+from dotenv import load_dotenv
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    db="to-do-list-python",
-    autocommit=True
-)
+load_dotenv()
+
+db_config = {
+    "host": os.getenv("host"),
+    "user": os.getenv("user"),
+    "password": os.getenv("password"),
+    "db": os.getenv("db"),
+}
+
+mydb = mysql.connector.connect(**db_config)
 
 mycursor = mydb.cursor(dictionary=True)
